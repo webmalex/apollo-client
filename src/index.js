9dcd3320169b45f61d455f760cb76e6e307700c6
Alexander Efimov <alexandr.efimov@gmail.com>
2017-02-24 19:04:34 +0500

    https://github.com/apollographql/frontpage-server

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+// const client = new ApolloClient();
+const client = new ApolloClient({
+  // networkInterface: createNetworkInterface({ uri: ':8080' }),
+  // connectToDevTools: true
+});
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
-);
+)
